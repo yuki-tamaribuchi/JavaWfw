74b976a85146f68179e7dece3aaee8a2473f68ad
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-07 00:03:03 +0900

    Create infterface for Urls class

---
+++ app/src/main/java/com/uktm/javawfw/urls/IUrls.java
@@ -0,0 +1,12 @@
+package com.uktm.javawfw.urls;
+
+import java.util.ArrayList;
+
+import com.uktm.javawfw.controller.base.IController;
+import com.uktm.javawfw.urls.Path;
+
+
+public interface IUrls {
+
+	public IPath getPath(String url);
+}
