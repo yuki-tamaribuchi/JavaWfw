10c0abf83e42a1d7e9fe9aa77a5f78eb0fd26ac5
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-07 00:00:37 +0900

    Create getUrlsArrayList method

--- app/src/main/java/com/uktm/javawfw/JavaWfw.java
+++ app/src/main/java/com/uktm/javawfw/JavaWfw.java
@@ -1,18 +1,30 @@
 package com.uktm.javawfw;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.uktm.javawfw.server.Server;
+import com.uktm.javawfw.urls.IUrls;
 
 
 public class JavaWfw {
-	public JavaWfw() {
-		try{
-            Server server = new Server("127.0.0.1", 8000);
+    private ArrayList<IUrls> urlsArrayList = new ArrayList<IUrls>();
+
+    public void serve() {
+        try{
+            Server server = new Server("127.0.0.1", 8000, this);
             server.serve();
         } catch (IOException e) {
             System.err.println("Host error: " + e);
         }
-	}
-	
+    }
+
+    public void addUrls(IUrls urls) {
+        urlsArrayList.add(urls);
+    }
+
+    public ArrayList<IUrls> getUrlsArrayList() {
+        return urlsArrayList;
+    }
 }
