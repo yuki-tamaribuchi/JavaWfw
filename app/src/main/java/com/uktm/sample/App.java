4a5dcad961c7f8123dd92367cc395d772aaf5392
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-07 00:07:17 +0900

    Modify App

--- app/src/main/java/com/uktm/sample/App.java
+++ app/src/main/java/com/uktm/sample/App.java
@@ -1,9 +1,18 @@
 package com.uktm.sample;
 
 import com.uktm.javawfw.JavaWfw;
+import com.uktm.javawfw.urls.IUrls;
+
+
+import com.uktm.sample.Urls;
 
 public class App {
 	public static void main(String[] args) {
+        IUrls urls = new Urls();
+
         JavaWfw app = new JavaWfw();
+        app.addUrls(urls);
+
+        app.serve();
     }
 }
