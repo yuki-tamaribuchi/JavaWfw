38313c158e99c0ab977139fa9bafb0daebae9608
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-07 00:04:18 +0900

    Modify Worker class

--- app/src/main/java/com/uktm/javawfw/server/Worker.java
+++ app/src/main/java/com/uktm/javawfw/server/Worker.java
@@ -4,9 +4,13 @@ import com.uktm.javawfw.http.request.Request;
 import com.uktm.javawfw.http.request.IRequest;
 import com.uktm.javawfw.exception.http.request.LoadRequestFailedException;
 import com.uktm.javawfw.http.response.IResponse;
+import com.uktm.javawfw.urls.URLResolver;
+import com.uktm.javawfw.exception.urls.URLMatchNotFoundException;
+import com.uktm.javawfw.urls.IPath;
 
 import com.uktm.sample.controllers.SampleController;
 import com.uktm.javawfw.controller.base.IController;
+import com.uktm.javawfw.JavaWfw;
 
 
 import java.net.*;
@@ -20,21 +24,36 @@ public class Worker extends Thread {
 	private Socket socket;
 	private IRequest request;
 	private IResponse response;
+	private URLResolver urlResolver;
 
-
-	public Worker(Socket socket){
+	public Worker(Socket socket, URLResolver urlResolver){
 		this.socket = socket;
+		this.urlResolver = urlResolver;
 	}
 
-
 	public void run() {
+		IPath path = null;
+		Class<? extends IController> controllerClass = null;
+		IController controller = null;
+		
 		try{
 			request = new Request(socket);
 			System.out.println(request.getRequestOutput());
 
+			try {
+				path = urlResolver.resolve(request.getRequestLine().get("uri"));
+			} catch (URLMatchNotFoundException e) {
+				System.out.println("not found");
+			}
 
-			IController contorller = new SampleController();
-			IResponse response = contorller.get(request);
+			controllerClass = path.getController();
+			try {
+				controller = controllerClass.getDeclaredConstructor().newInstance();
+			} catch (Exception e) {
+				System.out.println("New Instance error: " + e);
+			}
+
+			IResponse response = controller.get(request);
 			response.sendResponse();
 
 			socket.close();
