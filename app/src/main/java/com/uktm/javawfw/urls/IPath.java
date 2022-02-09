553532d691139fd544e84cd4246a42cc36af40cd
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-10 00:00:55 +0900

    Add requestMiddlewares and responseMiddlewares

--- app/src/main/java/com/uktm/javawfw/urls/IPath.java
+++ app/src/main/java/com/uktm/javawfw/urls/IPath.java
@@ -1,8 +1,11 @@
 package com.uktm.javawfw.urls;
 
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 
 import com.uktm.javawfw.controller.base.IController;
+import com.uktm.javawfw.middleware.request.base.IRequestMiddleware;
+import com.uktm.javawfw.middleware.response.base.IResponseMiddleware;
 
 public interface IPath {
 	public String getUrl();
@@ -10,4 +13,6 @@ public interface IPath {
 	public boolean isUrlMatched(String url);
 	public Class<? extends IController> getController();
 	public String getName();
+	public ArrayList<Class<? extends IRequestMiddleware>> getRequestMiddlewares();
+	public ArrayList<Class<? extends IResponseMiddleware>> getResponseMiddlewares();
 }
--- app/src/main/java/com/uktm/javawfw/urls/Path.java
+++ app/src/main/java/com/uktm/javawfw/urls/Path.java
@@ -1,20 +1,28 @@
 package com.uktm.javawfw.urls;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.uktm.javawfw.controller.base.IController;
 import com.uktm.javawfw.urls.IPath;
+import com.uktm.javawfw.middleware.request.base.IRequestMiddleware;
+import com.uktm.javawfw.middleware.response.base.IResponseMiddleware;
+
 
 public class Path implements IPath {
 	private String url;
 	private Class<? extends IController> controller;
 	private String name;
+	private ArrayList<Class<? extends IRequestMiddleware>> requestMiddlewares;
+	private ArrayList<Class<? extends IResponseMiddleware>> responseMiddlewares;
 
-	public Path(String url, Class<? extends IController> controller, String name) {
+	public Path(String url, Class<? extends IController> controller, String name, ArrayList<Class<? extends IRequestMiddleware>> requestMiddlewares, ArrayList<Class<? extends IResponseMiddleware>> responseMiddlewares) {
 		this.url = url;
 		this.controller = controller;
 		this.name = name;
+		this.requestMiddlewares = requestMiddlewares;
+		this.responseMiddlewares = responseMiddlewares;
 	}
 
 	public String getUrl() {
@@ -39,5 +47,13 @@ public class Path implements IPath {
 
 	public String getName() {
 		return name;
+	}
+
+	public ArrayList<Class<? extends IRequestMiddleware>> getRequestMiddlewares() {
+		return requestMiddlewares;
+	}
+
+	public ArrayList<Class<? extends IResponseMiddleware>> getResponseMiddlewares() {
+		return responseMiddlewares;
 	}
 }
