2d998c4def37251acb7b2ae5b6b3b6ece27d6cfc
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-11 00:01:45 +0900

    Add validateUrls method

--- app/src/main/java/com/uktm/javawfw/urls/AbstractUrls.java
+++ app/src/main/java/com/uktm/javawfw/urls/AbstractUrls.java
@@ -4,6 +4,7 @@ import com.uktm.javawfw.controller.base.IController;
 import com.uktm.javawfw.urls.IUrls;
 import com.uktm.javawfw.urls.IPath;
 import com.uktm.javawfw.urls.Path;
+import com.uktm.javawfw.exception.urls.URLRegexNotMatchedException;
 
 import java.util.ArrayList;
 
@@ -11,6 +12,12 @@ import java.util.ArrayList;
 public abstract class AbstractUrls implements IUrls {
 	public ArrayList<IPath> urlPatterns = new ArrayList<IPath>();
 
+	public void validateUrls() throws URLRegexNotMatchedException {
+		int i;
+		for(i=0; i<=urlPatterns.size()-1; i++) {
+			urlPatterns.get(i).validateUrl();
+		}
+	}
 
 	private IPath matchUrl(String url) {
 		int i;
--- app/src/main/java/com/uktm/javawfw/urls/IUrls.java
+++ app/src/main/java/com/uktm/javawfw/urls/IUrls.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 import com.uktm.javawfw.controller.base.IController;
 import com.uktm.javawfw.urls.Path;
+import com.uktm.javawfw.exception.urls.URLRegexNotMatchedException;
 
 
 public interface IUrls {
-
+	public void validateUrls() throws URLRegexNotMatchedException;
 	public IPath getPath(String url);
 }
