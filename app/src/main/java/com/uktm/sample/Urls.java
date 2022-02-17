e877b6c1d8cb1ca87876636b6361cda14c4c229f
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-18 08:11:10 +0900

    Modify urls

--- app/src/main/java/com/uktm/sample/Urls.java
+++ app/src/main/java/com/uktm/sample/Urls.java
@@ -7,12 +7,16 @@ import com.uktm.javawfw.urls.Path;
 import com.uktm.javawfw.urls.AbstractUrls;
 
 import com.uktm.sample.controllers.SampleController;
+import com.uktm.javawfw.middleware.request.base.IRequestMiddleware;
+import com.uktm.javawfw.middleware.request.security.AllowedHostMiddleware;
 
 
 public class Urls extends AbstractUrls {
 	public Urls() {
-		urlPatterns.add(new Path("sample/", SampleController.class, "sample"));
-		urlPatterns.add(new Path("sample2/<username>/", SampleController.class, "sample"));
-		urlPatterns.add(new Path("sample3/<username>/entry/<id>/", SampleController.class, "sample"));
+		ArrayList<Class<? extends IRequestMiddleware>> requestMiddlewares = new ArrayList<Class<? extends IRequestMiddleware>>();
+		requestMiddlewares.add(AllowedHostMiddleware.class);
+		urlPatterns.add(new Path("sample/", SampleController.class, "sample", requestMiddlewares, null));
+		urlPatterns.add(new Path("sample2/<username>/", SampleController.class, "sample", requestMiddlewares, null));
+		urlPatterns.add(new Path("sample3/<username>/entry/<id>/", SampleController.class, "sample", requestMiddlewares, null));
 	}
 }
