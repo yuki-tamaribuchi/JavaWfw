aae2fef1f8f57675ad46da235cecbe8c9663eaf5
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-25 10:38:57 +0900

    Acreate AbstractResposneMiddleware class

---
+++ app/src/main/java/com/uktm/javawfw/middleware/response/base/AbstractResponseMiddleware.java
@@ -0,0 +1,6 @@
+package com.uktm.javawfw.middleware.response.base;
+
+import com.uktm.javawfw.middleware.response.base.IResponseMiddleware;
+
+
+public abstract class AbstractResponseMiddleware implements IResponseMiddleware {}
