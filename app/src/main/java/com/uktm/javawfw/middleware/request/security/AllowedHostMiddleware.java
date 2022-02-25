861bdc889e4451beb7c3410938e8b307e6752975
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-25 10:38:15 +0900

    Create AllowedHostMiddleware class

--- app/src/main/java/com/uktm/javawfw/middleware/request/security/AllowedHostMiddleware.java
+++ app/src/main/java/com/uktm/javawfw/middleware/request/security/AllowedHostMiddleware.java
@@ -1,18 +1,15 @@
 package com.uktm.javawfw.middleware.request.security;
 
 import java.security.DrbgParameters.Reseed;
+import java.util.Hashtable;
 
 import com.uktm.javawfw.http.request.IRequest;
 import com.uktm.javawfw.middleware.request.base.AbstractRequestMiddleware;
 
 
 public class AllowedHostMiddleware extends AbstractRequestMiddleware {
-	public AllowedHostMiddleware(IRequest request) {
-		super(request);
-	}
 
-	public IRequest execute() {
-		System.out.println(request.getRequestLine());
+	public static IRequest execute(IRequest request) {
 		return request;
 	}
 }
