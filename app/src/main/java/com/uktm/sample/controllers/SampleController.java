134c13a383d96ef5754446604e426480d7963138
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-27 00:20:35 +0900

    Update SampleController class

--- app/src/main/java/com/uktm/sample/controllers/SampleController.java
+++ app/src/main/java/com/uktm/sample/controllers/SampleController.java
@@ -3,15 +3,14 @@ package com.uktm.sample.controllers;
 import com.uktm.javawfw.http.request.IRequest;
 import com.uktm.javawfw.http.response.IResponse;
 import com.uktm.javawfw.http.response.JsonResponse;
-
-import java.util.Hashtable;
-
 import com.uktm.javawfw.controller.base.AbstractController;
 
 
 public class SampleController extends AbstractController {
 	public IResponse get(IRequest request) {
-		
-		return new JsonResponse(request.getSocket(), "200 OK", null, "");
+		String responseBody = request.getRequestLine().get("uri");
+
+
+		return new JsonResponse(request.getSocket(), "200 OK", null, responseBody);
 	}
 }
