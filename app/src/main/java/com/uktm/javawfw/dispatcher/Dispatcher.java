c8621bd8b1980e83dc6d0f73359877684c7f6619
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-17 00:03:48 +0900

    Add post, put and delete method

--- app/src/main/java/com/uktm/javawfw/dispatcher/Dispatcher.java
+++ app/src/main/java/com/uktm/javawfw/dispatcher/Dispatcher.java
@@ -3,19 +3,36 @@ package com.uktm.javawfw.dispatcher;
 import com.uktm.javawfw.dispatcher.IDispatcher;
 import com.uktm.javawfw.http.request.IRequest;
 import com.uktm.javawfw.http.response.IResponse;
+import com.uktm.javawfw.exception.http.response.NullResponseException;
 
 import java.lang.reflect.InvocationTargetException;
 
 import com.uktm.javawfw.controller.base.IController;
 
 public class Dispatcher implements IDispatcher {
-	public static IResponse dispatch(IRequest request, Class<? extends IController> controllerClass){
+	public static IResponse dispatch(IRequest request, Class<? extends IController> controllerClass) throws NullResponseException {
 		IResponse response = null;
 		IController controller = null;
 
 		try {
 			controller = controllerClass.getDeclaredConstructor().newInstance();
-			response = controller.get(request);
+			String requestMethod = request.getRequestLine().get("method");
+			if (requestMethod.equals("GET")){
+				response = controller.get(request);
+			}
+			else if (requestMethod.equals("POST")){
+				response = controller.post(request);
+			}
+			else if (requestMethod.equals("PUT")){
+				response = controller.put(request);
+			}
+			else if (requestMethod.equals("DELETE")){
+				response = controller.delete(request);
+			}
+
+			if (response==null){
+				throw new NullResponseException();
+			}
 		} catch(InstantiationException|NoSuchMethodException|IllegalAccessException|InvocationTargetException e) {
 			System.out.println("Dispatcher error: " + e);
 		}
