cf98f239890661f42c68fb7f7e1ac6fda8869258
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-20 08:38:13 +0900

    Modify JsonResponse

--- app/src/main/java/com/uktm/javawfw/http/response/JsonResponse.java
+++ app/src/main/java/com/uktm/javawfw/http/response/JsonResponse.java
@@ -2,14 +2,18 @@ package com.uktm.javawfw.http.response;
 
 import java.net.Socket;
 import java.util.Hashtable;
+import java.util.HashMap;
+import org.json.JSONObject;
 
 
 import com.uktm.javawfw.http.response.AbstractResponse;
 
 
 public class JsonResponse extends AbstractResponse {
-	public JsonResponse(Socket socket, String status, Hashtable<String, String> responseOptions, String responseBody) {
-		super(socket, status, responseOptions, responseBody);
+	public JsonResponse(Socket socket, String status, Hashtable<String, String> responseOptions, HashMap<String, String> jsonResponseBodyHashMap) {
+		super(socket, status, responseOptions, null);
 		this.responseOptions.put("Content-Type", "application/json");
+		JSONObject jsonResponseBodyObject = new JSONObject(jsonResponseBodyHashMap);
+		this.responseBody = jsonResponseBodyObject.toString();
 	}
 }
