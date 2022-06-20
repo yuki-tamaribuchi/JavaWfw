92d48eae250fd51b1b9db13b6131a35b5d9cbb81
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-21 00:14:34 +0900

    Create HttpStatus

---
+++ app/src/main/java/com/uktm/javawfw/http/response/HttpStatus.java
@@ -0,0 +1,16 @@
+package com.uktm.javawfw.http.response;
+
+public class HttpStatus {
+	public static String OK = "200 OK";
+	public static String CREATED = "201 Created";
+	public static String ACCEPTED = "202 Accepted";
+	public static String NO_CONTENT = "204 No Content";
+	public static String BAD_REQUEST = "400 Bad Request";
+	public static String UNAUTHORIZED = "401 Unauthorized";
+	public static String FORBIDDEN = "403 Forbidden";
+	public static String NOT_FOUND = "404 Not Found";
+	public static String METHOD_NOT_ALLOWED = "405 Method Not Allowed";
+	public static String NOT_ACCEPTABLE = "406 Not Acceptable";
+	public static String CONFLICT = "409 Conflict";
+	public static String INTERNAL_SERVER_ERROR = "500 Internal Server Error";
+}
