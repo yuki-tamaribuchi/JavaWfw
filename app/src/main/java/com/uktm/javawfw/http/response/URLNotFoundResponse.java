764ab9d5ad864f8a60ab3406a915f2b5e35b14e5
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-21 00:15:07 +0900

    Modify status

--- app/src/main/java/com/uktm/javawfw/http/response/URLNotFoundResponse.java
+++ app/src/main/java/com/uktm/javawfw/http/response/URLNotFoundResponse.java
@@ -4,10 +4,11 @@ import java.net.Socket;
 import java.util.Hashtable;
 
 import com.uktm.javawfw.http.response.AbstractResponse;
+import com.uktm.javawfw.http.response.HttpStatus;
 
 
 public class URLNotFoundResponse extends AbstractResponse {
 	public URLNotFoundResponse(Socket socket) {
-		super(socket, "404 Not Found", null, null);
+		super(socket, HttpStatus.NOT_FOUND, null, null);
 	}
 }
