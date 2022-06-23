fa51aa651b4538aafa99d45ea3d2c06cabf8da37
TAMARIBUCHI <uk.tmbc2408@gmail.com>
2022-06-24 00:02:56 +0900

    Modify App

--- app/src/main/java/com/uktm/sample/App.java
+++ app/src/main/java/com/uktm/sample/App.java
@@ -8,9 +8,11 @@ import com.uktm.sample.Urls;
 
 public class App {
 	public static void main(String[] args) {
+        String serverAddr = "127.0.0.1";
+        int serverPort = 8000;
         IUrls urls = new Urls();
 
-        JavaWfw app = new JavaWfw();
+        JavaWfw app = new JavaWfw(serverAddr, serverPort);
         app.addUrls(urls);
 
         app.serve();
