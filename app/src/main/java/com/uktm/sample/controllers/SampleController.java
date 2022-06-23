18b2f5b78969f84a061da9f8014e1fd43280159c
TAMARIBUCHI <uk.tmbc2408@gmail.com>
2022-06-24 00:03:15 +0900

    Modify SampleController

--- app/src/main/java/com/uktm/sample/controllers/SampleController.java
+++ app/src/main/java/com/uktm/sample/controllers/SampleController.java
@@ -4,13 +4,17 @@ import com.uktm.javawfw.http.request.IRequest;
 import com.uktm.javawfw.http.response.IResponse;
 import com.uktm.javawfw.http.response.JsonResponse;
 import com.uktm.javawfw.controller.base.AbstractController;
+import com.uktm.javawfw.http.response.HttpStatus;
 
+import java.util.HashMap;
 
 public class SampleController extends AbstractController {
 	public IResponse get(IRequest request) {
-		String responseBody = request.getRequestLine().get("uri");
+		String message = request.getRequestLine().get("uri") + "が呼ばれました。";
 
+		HashMap<String, String> jsonResponseBodyHashMap = new HashMap<>();
+		jsonResponseBodyHashMap.put("message", message);
 
-		return new JsonResponse(request.getSocket(), "200 OK", null, responseBody);
+		return new JsonResponse(request.getSocket(), HttpStatus.OK, null, jsonResponseBodyHashMap);
 	}
 }
